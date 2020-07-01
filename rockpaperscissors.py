546f86cc18f7522d2e7d1589b61ef4e4375d7d2f
2017315018_Bangjeho <fix9111@naver.com>
2020-07-01 13:58:04 +0900

    [winning-rule-01] Add the winning rule

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,29 +1,20 @@
 # list of play options
 play = ["Rock", "Paper", "Scissors"]
+# game rule
+winning_rule = {"Rock": "Scissors", "Paper": "Rock",
+                 "Scissors": "Papper"}
 # assign a random play to the computer
 computer = "Rock"
 print('Computer: {}'.format(computer))
 # get the user input
-player = "Paper"
+player = input("Rock,Paper, Scissors? ")
 print('Player: {}'.format(player))
 # tie
 if player == computer:
      print("Tie!")
-# rock
-elif player == "Rock":
-     if computer == "Scissors":
-         print("You win!")
-     else:
-         print("You lose!")
-# paper
-elif player == "Paper":
-     if computer == "Rock":
-         print("You win!")
-     else:
-         print("You lose!")
-# scissors
-elif player == "Scissors":
-     if computer == "Paper":
-         print("You win!")
-     else:
-         print("You lose!")
+# you win
+elif winning_rule[player] == computer:
+     print("You win!")
+# you lose
+else:
+     print("You lose!")
